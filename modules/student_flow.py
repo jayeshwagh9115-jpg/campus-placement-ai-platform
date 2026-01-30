04477dfb9c451274e59ff7a9aeb1b0e72fcd62ad
jayeshwagh9115-jpg <jayeshwagh9115@gmail.com>
2026-01-30 10:55:34 +0530

    Update student_flow.py

--- modules/student_flow.py
+++ modules/student_flow.py
@@ -513,8 +513,7 @@ class StudentFlow:
             if st.button("◀ Previous Step"):
                 self.current_step = 1
                 st.rerun()
-                
-         with col3:
+        with col3:
             if st.button("Next Step ▶"):
                 self.current_step = 3
                 st.rerun()
